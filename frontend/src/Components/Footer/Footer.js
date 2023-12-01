f27c27877f71e04a950176b53a7ec7cfa5d3c74d
arshdeep811 <arshdeepsingh.cse19@chitkarauniversity.edu.in>
2023-12-02 01:14:59 +0530

    Updated Footer

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -5,21 +5,27 @@ const Footer = () => {
   return (
     <div>
       <footer className="footer-1">
-          <ul>
-            <li>About us</li>
-            <li>Contact</li>
-            <li>For Query</li>
-            <li>Career</li>
-          </ul>
-          <ul className="icons">
-            <li><FontAwesomeIcon icon={faFacebook} color="white" className="icon"/></li>
-            <li><FontAwesomeIcon icon={faInstagram} color="white" className="icon"/></li>
-            <li><FontAwesomeIcon icon={faTwitter} color="white" className="icon"/></li>
-            <li><FontAwesomeIcon icon={faPinterest} color="white" className="icon"/></li>
+        <h2 className="logo">StashCart</h2>
+        <p className="disclaimer">
+           This is a sample website created for demonstration purposes only. Any information, products, or 
+           services presented on this site are fictional and not intended for actual use. Please do not proceed
+           with any payments or transactions. We appreciate your understanding.
+        </p>
+        <ul className="icons">
+           <li><FontAwesomeIcon icon={faFacebook} color="white" className="icon"/></li>
+           <li><FontAwesomeIcon icon={faInstagram} color="white" className="icon"/></li>
+           <li><FontAwesomeIcon icon={faTwitter} color="white" className="icon"/></li>
+           <li><FontAwesomeIcon icon={faPinterest} color="white" className="icon"/></li>
           </ul>
       </footer>
       <footer className="footer-2">
-        <p>Copyright &copy; 2022, StashCart.com</p>
+        <p className="copyright">Copyright &copy; 2022, StashCart</p>
+        <ul>
+          <li><a href="/" id="a-home">Home</a></li>
+          <li>About us</li>
+          <li>Contact</li>
+          <li>Career</li>
+        </ul>
       </footer>
     </div>
   );
